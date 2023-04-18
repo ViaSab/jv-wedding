1d3d1e59b2750d3e72ff581416c6043f1d8bf767
ViaSab <vianney.sabourin@hotmail.fr>
2023-04-18 10:43:11 -0400

    JVW: Add the Netlify Identity Widget

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Jillian and Vianney's wedding",
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+    },
+  },
+});

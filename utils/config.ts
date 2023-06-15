a305a4118648c7c98eefebd887dc20490cb07aaa
ViaSab <vianney.sabourin@hotmail.fr>
2023-06-15 15:48:08 -0400

    Adding content for our story

--- utils/config.ts
+++ utils/config.ts
@@ -1,10 +1,14 @@
 // Main menu of the website
 export const MENU = ["index", "photos", "our-story"];
 
-// Liste of photos for the Our Story page
+// List of photos for the Our Story page
 export const OUR_STORY_PHOTOS = [
+  "blue-mountain",
+  "france",
   "alberta",
   "tofino",
   "arizona",
+  "cambridge",
   "lake-superior",
+  "engagement",
 ];

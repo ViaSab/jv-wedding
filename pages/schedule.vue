fd6ba1a6ccbba1e2292ddbcea23ae82595bb8421
ViaSab <vianney.sabourin@hotmail.fr>
2023-07-05 14:19:57 -0400

    French translations + svg

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="center">
+  <div class="center schedule">
     <ContentDoc />
   </div>
 </template>
+
+<style lang="scss">
+.schedule {
+  img {
+    max-width: 180px;
+    max-height: 100px;
+    margin-bottom: 15px;
+  }
+}
+</style>
--- utils/config.ts
+++ utils/config.ts
@@ -3,7 +3,7 @@ export const MENU = ["index", "our-story", "schedule"];
 
 // List of photos for the Our Story page
 export const OUR_STORY_PHOTOS = [
-  "blue-mountain",
+  "first-date",
   "france",
   "alberta",
   "tofino",

77942804550f6cd203626b9ae5c7988cbe8f5f9d
ViaSab <vianney.sabourin@hotmail.fr>
2023-07-18 16:04:53 -0400

    French Content

--- utils/config.ts
+++ utils/config.ts
@@ -4,11 +4,15 @@ export const MENU = ["index", "our-story", "schedule"];
 // List of photos for the Our Story page
 export const OUR_STORY_PHOTOS = [
   "first-date",
+  "christmas",
   "france",
   "alberta",
   "tofino",
   "arizona",
   "cambridge",
   "lake-superior",
+  "quebec",
+  "home",
+  "bike-trip",
   "engagement",
 ];

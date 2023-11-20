072a7e2c87c1cba928e5a039f67c17d74581feb1
ViaSab <vianney.sabourin@hotmail.fr>
2023-11-19 20:49:09 -0500

    Contact Info

--- components/Base/Footer.vue
+++ components/Base/Footer.vue
@@ -1,8 +1,24 @@
+<script setup lang="ts">
+const currentYear = computed(() => {
+  return new Date().getFullYear();
+});
+</script>
+
 <template>
   <ElFooter>
     <ElIcon><ElIconMagicStick /></ElIcon>
     &nbsp;
     <span>{{ $t("footer.text") }}</span>
+    <span class="vertical-divider">|</span>
+    <ElIcon><ElIconMessage /></ElIcon>
+    &nbsp;
+    <span
+      ><a href="mailto:jillianplusvianney@gmail.com"
+        >jillianplusvianney@gmail.com</a
+      ></span
+    >
+    <span class="vertical-divider">|</span>
+    <span>{{ currentYear }}</span>
   </ElFooter>
 </template>
 

d500ac44be878ee0fac43db530b5135c40b968c8
ViaSab <vianney.sabourin@hotmail.fr>
2024-01-28 16:50:30 -0500

    Small fixes

---
+++ components/content/BlockText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-row justify="center" :gutter="30">
+    <el-col :xs="24" :sm="20" :md="16" :lg="12">
+      <hr />
+      <ContentSlot :use="$slots.default" />
+      <hr />
+    </el-col>
+  </el-row>
+</template>
